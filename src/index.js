9f056ab1712ee0c2171d6df23d29649f1b62ca70
SLatyankov <Slatiankov@yandex.ru>
2020-03-18 12:15:41 +0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,35 @@
 
 exports.min = function min (array) {
-  return 0;
+    if(array === undefined || array.length === 0){
+        return 0;
+    }
+    var min1 = array[0];
+    array.forEach(element => {
+        if(element<min1){
+            min1 = element;
+        }
+    });
+  return min1;
 }
 
 exports.max = function max (array) {
-  return 0;
+        if (array === undefined || array.length === 0) {
+            return 0;
+        }
+  var max1 = array[0];
+  array.forEach(element => {
+      if (element > max1) {
+          max1 = element;
+      }
+  });
+  return max1;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+        if (array === undefined || array.length === 0) {
+            return 0;
+        }
+ var avg = 0;
+ array.forEach(element => avg += element)
+ return avg/array.length;
 }
